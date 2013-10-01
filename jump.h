5b40521ad62a0cc8e80a80fd4333d0c236b28f3b
ysdeal <ysdeal@gmail.com>
2013-10-01 13:19:15 -0700

    Jump Game II

---
+++ jump.h
@@ -0,0 +1,31 @@
+/**
+ Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+For example:
+Given array A = [2,3,1,1,4]
+
+The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.) 
+*/
+class Solution {
+public:
+    int jump(int A[], int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int last = 0;
+        int curr = 0;
+        int ret = 0;
+        
+        for (int i = 0; i < n; i++){
+            if(i > last){
+                last = curr;
+                ++ ret;
+            }
+            curr = max(curr, i+A[i]);
+        }
+        return ret;
+    }
+};
