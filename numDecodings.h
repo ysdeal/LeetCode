161b86e95c7069ba2afa21dc99dfd62ab01163ef
ysdeal <ysdeal@gmail.com>
2013-10-06 20:43:05 -0700

    Decode Ways

---
+++ numDecodings.h
@@ -0,0 +1,33 @@
+/**
+ A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2. 
+ */
+class Solution {
+public:
+    int numDecodings(string s) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int n = s.size();
+        vector<int> d(n+1,0);
+        if(n==0) return 0;
+        d[0] = 1;
+        for(int i = 1; i <= n; i++){
+            if(s[i-1]!='0')
+                d[i] = d[i - 1];
+            if(i>=2 && ((s[i-1] <= '6' && s[i-2]== '2') || s[i-2] == '1')) 
+               d[i] += d[i-2];
+        }
+        return d[n];
+    }
+};
+
