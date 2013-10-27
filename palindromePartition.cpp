aa7f2f2528a092f5cb85eaf7bdd45b6324bce654
ysdeal <ysdeal@gmail.com>
2013-10-27 16:19:12 -0700

    Palindrome Partitioning I&II

--- palindromePartition.cpp
+++ palindromePartition.cpp
@@ -13,9 +13,42 @@ Return
 #include <iostream>
 #include <string>
 #include <vector>
+#include <climits>
 using namespace std;
 void partBack(string, int, vector<vector<string> >&, vector<int>&);
 bool isPalindrome(string, int, int);
+int mincut(string);
+int min (int a, int b) { return (a < b)? a : b; }
+int mincut(string s){
+	int n = s.length();
+	int L[n][n];
+	bool P[n][n];
+	for(int i = 0; i < n; i++){
+		L[i][i] = 0;
+		P[i][i] = true;
+	}
+
+	for(int len = 2; len <= n; len++){
+		for(int i = 0; i < n - len + 1; i++){
+			int j = i + len -1;
+			if(len == 2)
+				P[i][j] = (s[i] == s[j]);
+			else
+				P[i][j] = (s[i] == s[j]) && P[i+1][j-1];
+		    if(P[i][j])
+		    	L[i][j] = 0;
+		    else{
+		    	L[i][j] = INT_MAX;
+		    	for(int k = i; k < j; k++)
+		    		L[i][j] = min(L[i][j], L[i][k] + L[k+1][j] + 1);
+		    }
+		}
+	}
+	return L[0][n-1];
+}
+
+
+
 vector<vector<string> > partition(string s){
 	vector<vector<string> > res;
 	vector<int> index;
@@ -62,6 +95,6 @@ int main(int argc, char const *argv[])
 			cout << y << ",";
 		cout << endl;	
 	}
-
+    cout << mincut(s) << endl;
 	return 0;
 }
